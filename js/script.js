a48a154dfbe51a6ce9f7f0f0ea9288caef6bbbf6
rudraarxx <64283030+rudraarxx@users.noreply.github.com>
2022-07-05 18:02:00 +0530

    Interior Design

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
-        // items : 1, // THIS IS IMPORTANT
-        autoPlay: 3000,
-        responsive : {
-              480 : { items : 1  }, // from zero to 480 screen width 4 items
-              768 : { items : 1  }, // from 480 screen widthto 768 6 items
-              1024 : { items : 1   // from 768 screen width to 1024 8 items
-              }
-          },
-          items:4,
+    items : 1, // THIS IS IMPORTANT
     loop:true,
     margin:10,
+    animateOut: 'fadeOutDown',
+    animateIn: 'flipInX',
+    autoHeight:true,
+    dots: false,
+    smartSpeed:450,
     autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true
     });
 
   });
-
+  $(function() {
+    AOS.init();
+});
