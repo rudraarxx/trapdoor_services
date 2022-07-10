b71cfd8c83cc73902d531deea0dd577da887925e
rudraarxx <64283030+rudraarxx@users.noreply.github.com>
2022-07-11 03:47:53 +0530

    Interior Design website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
+    $(".m-slides").owlCarousel({
     items : 1, // THIS IS IMPORTANT
     loop:true,
     margin:10,
@@ -13,7 +13,53 @@ $(document).ready(function(){
     autoplayHoverPause:true
     });
 
-  });
-  $(function() {
+});
+$(function() {
     AOS.init();
+});
+
+const swiper = new Swiper('.second-pg-slider', {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        loop: true,
+        autoplay:{
+          delay: 2000,
+          disableOnInteraction: true,
+        },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+var swipe = new Swiper('.forth-pg-slider', {
+  direction: 'horizontal',
+  loop: true,
+  effect: "fade",
+  autoplay:{
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+    clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
